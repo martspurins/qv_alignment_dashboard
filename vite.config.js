7158fab7c2e2077c16418614c468ab263b423665
Marts Purins <purinsmarts@gmail.com>
2026-05-23 11:29:05 -0500

    Fix bundle splitting for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/qv_alignment_dashboard/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'react'
+          }
+          if (id.includes('node_modules/recharts')) {
+            return 'recharts'
+          }
+        }
+      }
+    }
+  }
 })
